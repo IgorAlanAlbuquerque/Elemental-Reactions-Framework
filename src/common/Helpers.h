d21c1db25a81264eba3b2a054e6afa7a4bc38627
igora <igoralbuquerque102@hotmail.com>
2025-11-26 17:49:06 -0300

    fix(po3): :bug: corrigido problema do editorID
    para poder buscar o editorID foi adicionado a dependência ao po3 tweks que lê e faz cache deles

--- src/common/Helpers.h
+++ src/common/Helpers.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <Windows.h>
+
 #include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <string>
 #include <type_traits>
+
+#include "RE/Skyrim.h"
 
 [[nodiscard]] consteval std::uint32_t FOURCC(char a, char b, char c, char d) {
     return (std::uint32_t(a) << 24) | (std::uint32_t(b) << 16) | (std::uint32_t(c) << 8) | std::uint32_t(d);
@@ -46,3 +51,35 @@ inline RE::Actor* AsActor(RE::MagicTarget* mt) noexcept {
 [[nodiscard]] constexpr double safeDiv(double a, double b) noexcept { return (b <= 0.0) ? 0.0 : (a / b); }
 
 [[nodiscard]] constexpr std::uint64_t MakeWidgetID(std::uint32_t formID) { return (std::uint64_t(formID) << 32); }
+
+namespace ERF {
+
+    using _PO3_GetFormEditorID = const char* (*)(std::uint32_t);
+
+    inline std::string GetEditorID_po3(const RE::TESForm* form) {
+        if (!form) {
+            return {};
+        }
+
+        // mesmo padrão que o exemplo da página do Tweaks
+        // https://www.nexusmods.com/skyrimspecialedition/mods/51073
+        static HMODULE tweaks = ::GetModuleHandleW(L"po3_Tweaks");
+        if (!tweaks) {
+            return {};
+        }
+
+        static auto func = reinterpret_cast<_PO3_GetFormEditorID>(::GetProcAddress(tweaks, "GetFormEditorID"));
+
+        if (!func) {
+            return {};
+        }
+
+        const char* ed = func(form->formID);
+        if (ed && *ed) {
+            return std::string(ed);
+        }
+
+        return {};
+    }
+
+}
